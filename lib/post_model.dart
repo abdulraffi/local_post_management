c5dc353af3270e1dc6cd6ef28c2587618d07fa35
Nandang Hermawan <nadanghermawan2014@gmail.com>
2023-09-06 12:40:29 +0700

    commit

--- lib/post_model.dart
+++ lib/post_model.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 
-class PostModel{
+class PostModel {
   Uri? url;
-  Map<String,String> headers = {};
-  Map<String,String> query = {};
-  Map<String,dynamic> body = {};
+  Map<String, String> headers = {};
+  Map<String, String> query = {};
+  Map<String, dynamic> body = {};
   DateTime? lastTryDate;
   String? lastError;
 
@@ -19,15 +19,29 @@ class PostModel{
 
   PostModel.fromJson(Map<String, dynamic> json) {
     url = Uri.parse(json['url']);
-    headers = json['headers'] == null ? {} : (json['headers']);
-    query = json['query'] == null ? {} : (json['query']);
-    body = json['body'] == null ? {} : (json['body']);
-    lastTryDate = json['lastTryDate'] == null ? null : DateTime.parse(json['lastTryDate']);
+    try {
+      headers = json['headers'];
+    } catch (e) {
+      headers = {};
+    }
+    try {
+      query = json['query'];
+    } catch (e) {
+      query = {};
+    }
+    try {
+      body = json['body'];
+    } catch (e) {
+      body = {};
+    }
+    lastTryDate = json['lastTryDate'] == null
+        ? null
+        : DateTime.parse(json['lastTryDate']);
     lastError = json['lastError'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data =  <String, dynamic>{};
+    final Map<String, dynamic> data = <String, dynamic>{};
     data['url'] = url.toString();
     data['headers'] = json.encode(headers);
     data['query'] = json.encode(query);
